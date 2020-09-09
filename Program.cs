6350761312105708264993997e945829a9e5be30
aetxabao <aitor.etxabarren@mariaanasanz.es>
2020-09-10 01:16:32 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace yosoy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yo soy Aitor.");//@aetxabao
+        }
+    }
+}
